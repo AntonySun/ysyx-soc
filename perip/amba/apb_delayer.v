1cb940f1564af8e141889185927518525fabbf75
ysyx_22050291-孙安东 <1044238861@qq.com>
2024-08-12 14:28:21 +0800

    soc_delay_fix

--- perip/amba/apb_delayer.v
+++ perip/amba/apb_delayer.v
@@ -23,16 +23,63 @@ module apb_delayer(
   input  [31:0] out_prdata,
   input         out_pslverr
 );
+ 
+  reg [15:0] r_num;
+  reg [11:0] r_counter;
+  reg okk;
+  reg [31:0] rdata;
+ 
 
   assign out_paddr   = in_paddr;
-  assign out_psel    = in_psel;
-  assign out_penable = in_penable;
+  assign out_psel    = okk ? in_psel : 0;
+  assign out_penable = okk ? in_penable : 0;
   assign out_pprot   = in_pprot;
   assign out_pwrite  = in_pwrite;
   assign out_pwdata  = in_pwdata;
   assign out_pstrb   = in_pstrb;
-  assign in_pready   = out_pready;
-  assign in_prdata   = out_prdata;
+  assign in_pready   = (in_paddr >= 32'ha0000000 && in_paddr < 32'hc0000000) ? (okk == 1 && r_counter == 1 && in_penable == 1) : out_pready;
+  assign in_prdata   = (in_paddr >= 32'ha0000000 && in_paddr < 32'hc0000000) ? rdata : out_prdata;
   assign in_pslverr  = out_pslverr;
 
+ 
+
+  always @(posedge clock) begin
+
+    if (in_paddr >= 32'ha0000000 && in_paddr < 32'hc0000000  ) begin
+      if (in_psel == 1 && out_pready == 0 && okk == 1)begin
+        r_num <= r_num + 16'd80;
+      r_counter <= r_num[15:4];
+      end
+      else if (okk == 0) begin
+      if (r_counter > 1) begin
+        r_counter <= r_counter - 1;  
+      end
+      else if (r_counter == 1)begin
+        r_num <= 0;
+      end
+    end
+    end
+    
+    else begin
+      r_counter <= 1;
+      r_num <= 0;
+    end
+
+  end
+
+always @(posedge clock) begin
+  if ( in_paddr >= 32'ha0000000 && in_paddr < 32'hc0000000) begin
+    if ((out_pready == 1 && in_penable == 1 && in_psel == 1)) begin   
+      okk <= 0;
+      rdata <= out_prdata;
+    end
+    else if (r_counter == 1 && okk == 0)
+      okk <= 1;
+  end
+  else begin
+    okk <= 1;
+  end
+end
+
+  
 endmodule
